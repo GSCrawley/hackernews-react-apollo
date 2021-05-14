6a438a4f1691a4fe5937c86501ea1e8ec380e1ba
Gideon Crawley <gscrawley@gmail.com>
2021-05-13 23:28:47 -0700

    implemented search function

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import LinkList from './LinkList';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
+import Search from './Search';
 
 
 const App = () => {
@@ -19,6 +20,8 @@ const App = () => {
             component={CreateLink}
           />
            <Route exact path="/login" component={Login} />
+           <Route exact path="/search" component={Search} />
+
         </Switch>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,6 @@ const Header = () => {
           new
         </Link>
         <div className="ml1">|</div>
-        <Link to="/top" className="ml1 no-underline black">
-          top
-        </Link>
-        <div className="ml1">|</div>
         <Link
           to="/search"
           className="ml1 no-underline black"
